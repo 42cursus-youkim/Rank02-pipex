7fd794db5dc3c0bea99820700ffe2c27e7a797f4
youkim005 <42.4.youkim@gmail.com>
2021-08-14 13:37:02 +0900

    chore: :truck: compress #2

--- libft/ft_math_ll.c
+++ libft/ft_math_ll.c
@@ -1,39 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_math.c                                          :+:      :+:    :+:   */
+/*   ft_math_ll.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: youkim <youkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/08 11:38:52 by youkim            #+#    #+#             */
-/*   Updated: 2021/08/14 10:38:43 by youkim           ###   ########.fr       */
+/*   Updated: 2021/08/14 11:21:11 by youkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_min(int a, int b)
+long long	ft_lmin(long long a, long long b)
 {
 	if (a < b)
 		return (a);
 	return (b);
 }
 
-int	ft_max(int a, int b)
+long long	ft_lmax(long long a, long long b)
 {
 	if (a > b)
 		return (a);
 	return (b);
 }
 
-int	ft_abs(int n)
+long long	ft_labs(long long n)
 {
 	if (n >= 0)
 		return (n);
 	return (-n);
 }
 
-int	ft_sign(int n)
+int	ft_lsign(long long n)
 {
 	if (n < 0)
 		return (-1);
